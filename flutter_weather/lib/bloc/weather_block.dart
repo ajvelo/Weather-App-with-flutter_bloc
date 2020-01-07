7ae46086390275e13f87534d5a1ad2b18824332d
ajvelo <>
2020-01-07 12:45:11 +0000

    Add weather_bloc

---
+++ flutter_weather/lib/bloc/weather_block.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_weather/bloc/weather_event.dart';
+import 'package:flutter_weather/bloc/weather_state.dart';
+import 'package:flutter_weather/models/weather_model.dart';
+import 'package:flutter_weather/repository/weather_repository.dart';
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc({@required this.weatherRepository})
+      : assert(weatherRepository != null);
+
+  @override
+  WeatherState get initialState => WeatherEmpty();
+
+  @override
+  Stream<WeatherState> mapEventToState(
+    WeatherState currentState,
+    WeatherEvent event,
+  ) async* {
+    if (event is FetchWeather) {
+      yield WeatherLoading();
+      try {
+        final Weather weather = await weatherRepository.getWeather(event.city);
+        yield WeatherLoaded(weather: weather);
+      } catch (_) {
+        yield WeatherError();
+      }
+    }
+  }
+}
